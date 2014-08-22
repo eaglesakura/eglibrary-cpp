02dc7c7c453c19172408ca95d2f9d6155c020135
eaglesakura <eagle.sakura@gmail.com>
2014-08-23 01:04:20 +0900

    GLKit用ソースコードを追加

---
+++ eglibrary-android-glkit/jni/src/impl/es_glkit_GLKitUtil.cpp
@@ -0,0 +1,8 @@
+#include "es_glkit_GLKitUtil.h"
+extern "C" {
+
+JNIEXPORT jboolean JNICALL Java_com_eaglesakura_android_glkit_GLKitUtil_nativeCaptureDevice(JNIEnv *env, jobject device, jobject bitmap) {
+    return JNI_TRUE;
+}
+
+}
