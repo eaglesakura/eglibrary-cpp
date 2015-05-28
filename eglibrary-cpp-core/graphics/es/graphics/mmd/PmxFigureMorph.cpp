50aa2a196a548905ed9c77385198eabe93e91eb4
eaglesakura <eagle.sakura@gmail.com>
2015-05-29 01:43:25 +0900

    eglibraryに依存したMMD実装を削除

--- eglibrary-cpp-core/graphics/es/graphics/mmd/PmxFigureMorph.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by @eaglesakura on 15/05/22.
-//
-
-#include "PmxFigureMorph.h"
-
-namespace es {
-
-}
--- eglibrary-cpp-core/graphics/es/graphics/mmd/PmxMaterial.cpp
+++
@@ -1,6 +0,0 @@
-//
-// Created by @eaglesakura on 15/05/09.
-//
-
-//#include "PmxMaterial.h"
-// stub!
