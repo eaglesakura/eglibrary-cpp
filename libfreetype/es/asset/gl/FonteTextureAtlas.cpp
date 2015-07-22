cb21f082abf69d0f9edbd92f4f6001f2268a1667
eaglesakura <eagle.sakura@gmail.com>
2015-07-22 09:51:25 +0900

    テクスチャアトラスの焼きこみを暫定実装

--- libfreetype/es/asset/gl/FonteTextureAtlas.cpp
+++ libfreetype/es/asset/gl/FonteTextureAtlas.cpp
@@ -1,17 +1,181 @@
 #include "FonteTextureAtlas.h"
+#include "es/asset/FontCharactor.h"
 
 namespace es {
 
-FontTextureAtlas::FontTextureAtlas(const std::shared_ptr<FontFace> newFont) : font(newFont) {
-    assert(font);
+namespace internal {
+
+class BasicTextureAllocator : public FontTextureAtlas::TextureAllocator {
+    /**
+     * 生成するテクスチャのサイズ
+     */
+    uint size;
+
+public:
+    BasicTextureAllocator(uint newSize) : size(newSize) {
+        assert(isPowerOfTwo(newSize));
+        assert(newSize >= 2);
+    }
+
+    /**
+     * 新規にテクスチャを生成させる
+     *
+     * テクスチャは最低でもフォントの大きさ以上の解像度を持つ必要がある。
+     */
+    virtual std::shared_ptr<Texture> newTexture(sp<DeviceContext> context) {
+        sp<Texture> result(new Texture());
+        result->bind(context);
+        result->allocPixelMemory(PixelFormat_R8, 0, size, size);
+
+        const uint BUFFER_DIV = 8;
+        const uint BUFFER_SIZE = size / BUFFER_DIV;
+        std::vector<uint8_t> cache((BUFFER_SIZE) * (BUFFER_SIZE));
+        util::zeromemory(&cache);
+        for (int i = 0; i < BUFFER_DIV; ++i) {
+            for (int k = 0; k < BUFFER_DIV; ++k) {
+                // 空のバッファをアップロードしてテクスチャを空白にする
+                upload(result,
+                       BUFFER_SIZE * k, BUFFER_SIZE * i,
+                       BUFFER_SIZE, BUFFER_SIZE,
+                       unsafe_array<uint8_t>(util::asPointer(cache), cache.size())
+                );
+            }
+        }
+        result->unbind(context);
+        return result;
+    }
+
+    /**
+     * 画素情報のアップロードを行わせる
+     */
+    virtual void upload(std::shared_ptr<Texture> texture,
+                        const uint offsetX, const uint offsetY,
+                        const uint width, const uint height,
+                        const unsafe_array<uint8_t> &buffer) {
+        glTexSubImage2D(
+                GL_TEXTURE_2D,
+                0,
+                offsetX, offsetY,
+                width, height,
+                Pixel::toGLPixelFormat(PixelFormat_R8),
+                Pixel::toGLPixelDataType(PixelFormat_R8),
+                (void *) buffer.ptr
+        );
+    }
+
+};
+
 }
 
-uint FontTextureAtlas::bake(const std::wstring text) {
-    return 0;
+FontTextureAtlas::FontTextureAtlas(const std::shared_ptr<FontFace> newFont) : font(newFont) {
+    assert(font);
+
+    allocator.reset(new internal::BasicTextureAllocator(1024));
+}
+
+uint FontTextureAtlas::bake(std::shared_ptr<DeviceContext> context, const std::wstring text) {
+    class TempListener : public IImageDecodeListener {
+    public:
+        ImageInfo info;
+        unsafe_array<uint8_t> pixels;
+        ImageDecodeResult_e result = ImageDecodeResult_UnknownError;
+
+        virtual void onImageInfoDecoded(const ImageInfo *info) {
+            this->info = *info;
+        }
+
+        /**
+         * 画像を指定行読み込んだ
+         *
+         * 引数lineは使いまわされる可能性があるため、内部的にテクスチャコピー等を行うこと。
+         */
+        virtual void onImageLineDecoded(const ImageInfo *info, const unsafe_array<uint8_t> pixels, const uint height) {
+            this->pixels = pixels;
+        }
+
+        /**
+         * 画像のデコードをキャンセルする場合はtrue
+         */
+        virtual bool isImageDecodeCancel() {
+            return false;
+        }
+
+        /**
+         * デコードが完了した
+         */
+        virtual void onImageDecodeFinished(const ImageInfo *info, const ImageDecodeResult_e result) {
+            this->result = result;
+        }
+    } tempListener;
+
+    uint result = 0;
+    for (const wchar_t code : text) {
+        if (atlasMap.find(code) != atlasMap.end()) {
+            // 既にatlasを保持している
+            continue;
+        }
+
+        // 新規にAtlasを生成する
+        auto texture = textures[textures.size() - 1];
+        auto charactor = font->rendering(code, &tempListener);
+        if (charactor && tempListener.result == IImageDecodeListener::ImageDecodeResult_Success) {
+            // レンダリングに成功したので、テクスチャに焼きこむ
+            Vector2i16 bitmapSize = charactor->getBitmapSize();
+            if ((current.x + bitmapSize.x + 1) > current.x) {
+                // この行をはみ出す場合は改行する
+                current.y += (current.lineHeight + 1);
+                current.x = 0;
+                current.lineHeight = 0;
+            }
+            if ((current.y + bitmapSize.y) > current.y) {
+                // テクスチャからはみ出る場合はテクスチャも生成する
+                textures.push_back(allocator->newTexture(context));
+                texture = textures[textures.size() - 1];
+                assert(texture);
+                current.y = 0;
+            }
+
+            sp<FontArea> area(new FontArea());
+            area->area.left = current.x;
+            area->area.top = current.y;
+            area->area.right = area->area.left + tempListener.info.width;
+            area->area.bottom = area->area.top + tempListener.info.height;
+            area->charactor = charactor;
+            area->index = textures.size() - 1;
+
+            // 焼きこみを行う
+            {
+                texture->bind(context);
+                allocator->upload(texture,
+                                  area->area.left, area->area.top,
+                                  area->area.width(), area->area.height(),
+                                  tempListener.pixels
+                );
+                texture->unbind(context);
+            }
+
+            // 次のテクスチャ座標を設定
+            current.x += (area->area.width() + 1);
+            current.lineHeight = std::max<int16_t>(area->area.height() + 1, current.lineHeight);
+
+            // アトラス情報を保存
+            atlasMap.insert(std::pair<wchar_t, sp<FontArea> >(code, area));
+        }
+        ++result;
+    }
+
+    return result;
 }
 
 const std::vector<std::shared_ptr<es::Texture> > &FontTextureAtlas::getTextures() const {
     return textures;
 }
 
+void FontTextureAtlas::setAllocator(std::shared_ptr<TextureAllocator> allocator) {
+
+}
+
+const std::shared_ptr<FontTextureAtlas::FontArea> FontTextureAtlas::pick(const wchar_t charactor) const {
+    return sp<FontTextureAtlas::FontArea>();
+}
 }
--- libfreetype/es/asset/gl/FonteTextureAtlas.h
+++ libfreetype/es/asset/gl/FonteTextureAtlas.h
@@ -3,6 +3,7 @@
 #include "es/eglibrary.hpp"
 #include "es/graphics/gl/resource/Texture.h"
 #include "es/asset/FontFace.h"
+#include "es/memory/SafeArray.hpp"
 #include <vector>
 
 namespace es {
@@ -15,17 +16,28 @@ public:
 
     class FontArea;
 
+    class TextureAllocator;
+
     /**
      * テキストを焼きこむ
      *
      * 新規に焼きこまれたテクスチャ数を返却する
+     * bake中にgetTextures()するとiteratorが無効になる可能性があるため、外部でlockすることを推奨する。
      */
-    uint bake(const std::wstring text);
+    uint bake(std::shared_ptr<DeviceContext> context, const std::wstring text);
 
     /**
      * 管理しているテクスチャリストを取得する
      */
     const std::vector<std::shared_ptr<Texture> > &getTextures() const;
+
+    /**
+     * カスタムアロケータを生成する
+     *
+     * セットしない場合はデフォルトのアロケータが使用される。
+     * その場合、1枚のテクスチャで1024 x 1024の解像度を使用する。
+     */
+    void setAllocator(std::shared_ptr<TextureAllocator> allocator);
 
     /**
      * エリアをピックアップする。
@@ -61,12 +73,30 @@ public:
         friend class FontTextureAtlas;
     };
 
+    class TextureAllocator {
+    public:
+        /**
+         * 新規にテクスチャを生成させる
+         *
+         * テクスチャは最低でもフォントの大きさ以上の解像度を持つ必要がある。
+         * テクスチャの内部形式は1画素1byte単色(R8、A8等）である必要がある。
+         */
+        virtual std::shared_ptr<Texture> newTexture(std::shared_ptr<DeviceContext> context) = 0;
+
+        /**
+         * 画素情報のアップロードを行わせる
+         * メソッド呼び出し時点でTextureはバインド済みであるため、アップロードのみを行うこと。
+         */
+        virtual void upload(std::shared_ptr<Texture> texture, const uint offsetX, const uint offsetY, const uint width, const uint height, const unsafe_array<uint8_t> &buffer) = 0;
+
+        virtual ~TextureAllocator() = default;
+    };
+
 private:
     /**
      * 管理しているテクスチャ一覧
      */
     std::vector<sp<Texture> > textures;
-//    std::vector< <std::shared_ptr<Texture> >    textures;
 
     struct {
         /**
@@ -88,7 +118,12 @@ private:
     /**
      * 座標マップ
      */
-    std::map<wchar_t, sp<Texture> > atlasMap;
+    std::map<wchar_t, sp<FontArea> > atlasMap;
+
+    /**
+     * テクスチャ生成クラス
+     */
+    sp<TextureAllocator> allocator;
 
     /**
      * フォント情報
